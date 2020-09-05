c9b4156e8659f0b206518158df5e505f349c75f9
Diego Oliveira <diegooliveirabryto@gmail.com>
2020-09-05 10:55:58 -0300

    Challenge finished

--- ex025/ex025.js
+++ ex025/ex025.js
@@ -12,3 +12,11 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+function on(element, event, callback) {
+  document.querySelector(element).addEventListener(event, callback, false);
+}
+
+on('[data-js="select"]', 'change', function(){
+  document.querySelector('[data-js="input"]').value = this.value
+})
