9b20adf834298e69da51d6a20e377f4cce7fa7c3
Diego Oliveira <54894831+Diegooliveyra@users.noreply.github.com>
2020-09-11 12:51:28 -0300

    Update ex026.js

--- ex026/ex026.js
+++ ex026/ex026.js
@@ -1,5 +1,5 @@
-(function(){
-  'use stricts'
+(function () {
+  "use stricts";
   /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
@@ -19,31 +19,33 @@ selecionados.
 Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
-// ?
+  // ?
 
-function DOM(elements) {
-  this.element = document.querySelectorAll(elements)
-}
+  function DOM(elements) {
+    this.element = document.querySelectorAll(elements);
+  }
 
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
 
-DOM.prototype.on = function on(eventType, callback) {
+  var $a = new DOM('[data-js="link"]');
+  $a.on("click", function handleClick(e) {
+    e.preventDefault();
+    console.log("clicou");
+    $a.off('click', handleClick)
+  });
 
-}
-DOM.prototype.off = function off(eventType, callback) {
-
-}
-DOM.prototype.get = function get() {
-  return this.element;
-}
-
-
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
-
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
-
-})()
+  console.log("Elementos selecionados:", $a.get());
+  console.log("$a é filho de body?", $a.get()[0].parentNode === document.body);
+})();
