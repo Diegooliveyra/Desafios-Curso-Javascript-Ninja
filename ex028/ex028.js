e611f5250ab7e1e3dca24ea5b2b6db5ba3b957f5
Diego Oliveira <54894831+Diegooliveyra@users.noreply.github.com>
2020-09-18 12:55:58 -0300

    Challenge Update

--- ex028/ex028.js
+++ ex028/ex028.js
@@ -1,12 +1,50 @@
-(function(){
-    'use stricts'
+(function () {
+  "use stricts";
+  /*
+  No HTML:
+  - Crie um formulário com um input de texto que receberá um CEP e um botão
+  de submit;
+  - Crie uma estrutura HTML para receber informações de endereço:
+  "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
+  preenchidas com os dados da requisição feita no JS.
+  - Crie uma área que receberá mensagens com o status da requisição:
+  "Carregando, sucesso ou erro."
+  No JS:
+  - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
+  deve ser limpo e enviado somente os números para a requisição abaixo;
+  - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
+  "https://viacep.com.br/ws/[CEP]/json/", onde [CEP] será o CEP passado
+  no input criado no HTML;
+  - Essa requisição trará dados de um CEP em JSON. Preencha campos na tela
+  com os dados recebidos.
+  - Enquanto os dados são buscados, na área de mensagens de status, deve mostrar
+  a mensagem: "Buscando informações para o CEP [CEP]..."
+  - Se não houver dados para o CEP entrado, mostrar a mensagem:
+  "Não encontramos o endereço para o CEP [CEP]."
+  - Se houver endereço para o CEP digitado, mostre a mensagem:
+  "Endereço referente ao CEP [CEP]:"
+  - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
+  adicionar as informações em tela.
+  */
 
-    var ajax = new XMLHttpRequest()
-    ajax.open('GET', '/')
-    ajax.send()
+  var ajax = new XMLHttpRequest();
+  ajax.open("GET", "data.json");
+  ajax.send();
 
-    ajax.addEventListener('readystatechange', function(){
-        console.log('terminunou a requisição',ajax.readyState, ajax.status)
-    }, false)
+  console.log("Carregando ...");
 
-})()
+  ajax.addEventListener(
+    "readystatechange",
+    function () {
+      if (isRequestOk()) {
+        var data = JSON.parse(ajax.responseText);
+        console.log("Requisição Ok ", data.message);
+      }
+    },
+    false
+  );
+
+  function isRequestOk() {
+    return ajax.readyState === 4 && ajax.status === 200;
+  }
+})();
