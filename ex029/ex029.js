9f3d57e634c07107a87980ea74af5ea6ae8cb3cd
Diego Oliveira <diegooliveirabryto@gmail.com>
2020-09-26 10:20:55 -0300

    Challenge Update

--- ex029/ex029.js
+++ ex029/ex029.js
@@ -1,7 +1,7 @@
-(function() {
-    'use strict';
-  
-    /*
+(function () {
+  "use strict";
+
+  /*
     Vamos estruturar um pequeno app utilizando módulos.
     Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
     A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
@@ -31,14 +31,32 @@
     que será nomeado de "app".
     */
 
-    function app(){
-      return {
-        init: function(){
-          console.log(this)
-        }
-      }
+  function app() {
+    return {
+      init: function init() {
+        this.companyInfo();
+      },
 
-    }
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET", "company.json", true);
+        ajax.send();
+        ajax.addEventListener("readystatechange", this.getCompanyInfo, false);
+      },
+      getCompanyInfo: function getCompanyInfo() {
+        if (!app().isReady.call(this)) return;
 
-    app().init()
-  })();
+        var data = JSON.parse(this.responseText);
+        var $companyName = new DOM('[data-js="company-name"]');
+        var $companyPhone = new DOM('[data-js="company-phone"]');
+        $companyName.get()[0].textContent = data.name;
+        $companyPhone.get()[0].textContent = data.phone;
+      },
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      },
+    };
+  }
+
+  app().init();
+})();
